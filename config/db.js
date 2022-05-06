905931c8a3e16186463b0000107f14e4331a91d3
badouine <nzbadouine@gmail.com>
2022-05-06 05:45:58 +0000

    app connected

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://" +
+      process.env.DB_USER_PASS +
+      "@diwane.d8vjs.mongodb.net/diwane",
+    {
+      /*  useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false, */
+    }
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Failed to connect", err));
