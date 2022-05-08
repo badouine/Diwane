7a1317cc8088b8b59b631f02a899b41ea3eeaeb7
badouine <nzbadouine@gmail.com>
2022-05-08 03:30:29 +0000

    get all users

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth.controller');
-
-
+const userController = require('../controllers/user.controller');
+// auth 
 router.post('/register', authController.signUp);
 
+
+// info user
+router.get('/', userController.getAllUsers);
 module.exports = router;
