8304d257d034dd312b7b6996f4a41efc0ea06885
badouine <nzbadouine@gmail.com>
2022-05-09 04:57:37 +0000

    middleware auth

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require("express");
 require("dotenv").config({ path: "./config/.env" });
+const cookieParser = require('cookie-parser');
 require('./config/db');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/user.routes');
+const {checkUser} = require('./middleware/auth.middleware');
+
 const app = express();
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
+
+// jwt 
+app.get('*', checkUser);
 
 
 // routes 
