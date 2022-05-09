3ddc276d29cc0533d42d26103b787ccfe56abb05
badouine <nzbadouine@gmail.com>
2022-05-09 04:36:54 +0000

    login

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,7 +3,8 @@ const authController = require('../controllers/auth.controller');
 const userController = require('../controllers/user.controller');
 // auth 
 router.post('/register', authController.signUp);
-
+router.post('/login', authController.signIn);
+router.get('/logout', authController.logout);
 
 // info user
 router.get('/', userController.getAllUsers);
