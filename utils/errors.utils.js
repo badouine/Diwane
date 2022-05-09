83fb174c5bb2221ec096c26e32f6138f78151fb1
badouine <nzbadouine@gmail.com>
2022-05-09 07:42:33 +0000

    errors login

--- utils/errors.utils.js
+++ utils/errors.utils.js
@@ -1,8 +1,8 @@
 module.exports.signUpErrors = (err) => {
-    let errors = { pseudo: "", email: "", password: "" };
+    let errors = { tel, email: "", password: "" };
   
-    if (err.message.includes("pseudo"))
-      errors.pseudo = "Pseudo incorrect ou déjà pris";
+    if (err.message.includes("tel"))
+      errors.tel = "numero deja enregistre par un  autre";
   
     if (err.message.includes("email")) errors.email = "Email incorrect";
   
@@ -10,10 +10,23 @@ module.exports.signUpErrors = (err) => {
       errors.password = "Le mot de passe doit faire 6 caractères minium";
   
     if (err.code === 11000 && Object.keys(err.keyValue)[0].includes("pseudo"))
-      errors.pseudo = "Ce pseudo est déjà pris";
+      errors.tel = "Numero déjà pris entrer par autrui";
   
     if (err.code === 11000 && Object.keys(err.keyValue)[0].includes("email"))
       errors.email = "Cet email est déjà enregistré";
   
     return errors;
   };
+
+
+  module.exports.signInErrors = (err) => {
+    let errors = { email: '', password: ''}
+  
+    if (err.message.includes("email")) 
+      errors.email = "Email inconnu";
+    
+    if (err.message.includes('password'))
+      errors.password = "Le mot de passe ne correspond pas"
+  
+    return errors;
+  }
