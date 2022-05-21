f291f49f5d09bcaf6dbb399d3a8ebb9f6d2669b6
badouine <nzbadouine@gmail.com>
2022-05-21 02:13:46 +0000

    update profil

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -26,5 +26,19 @@ module.exports.uploadProfil = async(req, res) => {
         fs.createWriteStream(
             `${__dirname}/../client/public/uploads/profil/${fileName}`
         )
-    )
+    );
+
+    try {
+        await UserModel.findByIdAndUpdate(
+            req.body.userId,
+            {$set : {picture: "./uploads/profil" + fileName}},
+            {new: true, upsert: true, setDefaultsOnInsert: true},
+            (err, docs) => {
+                if (!err) return res.send(docs);
+                else return res.status(500).send({message: err})
+            }
+        );
+    } catch (err) {
+        return res.status(500).send({message: err});
+    }
 };
