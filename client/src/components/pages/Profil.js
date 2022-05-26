05c42fd9ba5612a341d47bff7896a9d869f0fe9b
badouine <nzbadouine@gmail.com>
2022-05-26 17:54:16 +0000

    context user if connected

--- client/src/components/pages/Profil.js
+++ client/src/components/pages/Profil.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Log from '../Log'
+import { UidContext } from "../AppContext";
 
 
 const Profil = () => {
+    const uid = useContext(UidContext);
     return (
         <div className="profil-page"> 
+        {uid ? (
+            <h1>UPDATE PAGE</h1>
+        ) : ( 
+
             <div className="log-container">
                 <Log signin={false} signup={true}/>
                 <div className="img-container">
                     <img src='./img/log.svg' alt=""/>
                  </div>
             </div>
+        ) }
         </div>
     );
 };
