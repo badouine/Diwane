2f3d4b6fef9690f5569bf4e01b36295cee104071
badouine <nzbadouine@gmail.com>
2022-06-04 09:18:14 +0000

    put card home

--- client/src/components/Thread.js
+++ client/src/components/Thread.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {getPosts} from '../actions/post.actions';
 import { useDispatch, useSelector } from 'react-redux';
 import { isEmpty } from './Utils';
+import Card from './Post/Card';
 
 
 const Thread = () => {
@@ -23,7 +24,7 @@ const Thread = () => {
         <ul>
             {!isEmpty(posts[0]) && 
                 posts.map((post) => {
-                    return <li> {post._id} </li>;
+                    return <Card post={post} key={post._id}/>;
                 }    
             )}
         </ul>
